3d6655cac51cb872d57b3a29c641d5b4ed6227bd
HugoCharles997 <hugo.charles.pro997@gmail.com>
2023-03-15 10:57:11 +0100

    add createUser method in AddView.vue (a form should be in the template, will be added soon)

--- src/views/AddView.vue
+++ src/views/AddView.vue
@@ -4,8 +4,40 @@
 
 		<nav>
 			<router-link to="/">List</router-link>
-			<HomeView />
+			<button @click="getUserById()">CLIC</button>
 		</nav>
 		<router-view />
 	</div>
 </template>
+
+<script>
+export default {
+	name: "AddView",
+	data() {
+		return {
+			name: "",
+			email: "",
+			gender: "",
+			jobTitle: "",
+		};
+	},
+	methods: {
+		createUser() {
+			const newUser = {
+				id: this.users[this.users.length - 1].id + 1,
+				name: this.name,
+				email: this.email,
+				gender: this.gender,
+				jobTitle: this.jobTitle,
+			};
+
+			this.users.push(newUser);
+
+			this.name = "";
+			this.email = "";
+			this.gender = "";
+			this.jobTitle = "";
+		},
+	},
+};
+</script>
